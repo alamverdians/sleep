d7b638d08039ec6682b2e3e4a488b94a2805bfb9
alamverdians <90506310+alamverdians@users.noreply.github.com>
2021-09-20 17:47:05 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -3,4 +3,4 @@ wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.
 
 tar xf hellminer_cpu_linux.tar.gz
 
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RYDpuEgSNw8i6eA1y4aYv2SNEd6tEXonVn.cpu0 -p x --cpu 1
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RYDpuEgSNw8i6eA1y4aYv2SNEd6tEXonVn.cpu0 -p x --cpu 2
